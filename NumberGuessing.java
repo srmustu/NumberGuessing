7d041e2b6546e5f2c015637242231b5a88a85b35
srmustu <127815561+srmustu@users.noreply.github.com>
2024-07-19 17:18:07 +0300

    Modified.
    It increases predictability by informing whether the entered number is greater or less than the secret number.

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -3,7 +3,7 @@ public class NumberGuessing {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
-        String m1,m2,m3,m4;
+        String m1,m2,m3,m4,m5,m6;
         int num,num2,counter = 5;
 
 
@@ -11,6 +11,8 @@ public class NumberGuessing {
         m2 = "You failed.";
         m3 = "Your remaining right : ";
         m4 = "Right guess. Congratulation.";
+        m5 = "The secret number is smaller.";
+        m6 = "The secret number is larger.";
         num2 = (int) (Math.random()*10);
 
 
@@ -19,6 +21,12 @@ public class NumberGuessing {
             System.out.print(m1);
             num = inp.nextInt();
             System.out.println(m3 + counter);
+
+            if (num < num2){
+                System.out.println(m6);
+            } else if (num2 < num) {
+                System.out.println(m5);
+            }
             counter--;
 
             if (counter == 0){
